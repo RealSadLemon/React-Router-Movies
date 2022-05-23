0193c23bfeef07c2874595a162eb68fb99466123
RealSadLemon <esmodearburk@gmail.com>
2022-05-23 16:55:13 -0500

    Added Router, and imported Link, Route, and Switch to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Link, Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -14,6 +14,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response.data);
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
